7843d367f6e453b7e20b5a3975d61c36c0a540f0
Wiktor Pink <wiktor.pink99@gmail.com>
2020-09-25 15:47:10 +0200

    initialize

---
+++ app/src/main/java/com/pink/swoosh/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pink.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
