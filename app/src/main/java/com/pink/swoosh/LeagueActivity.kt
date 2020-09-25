7d11c269de64fd8a08f5b9b1228d7c7f8e9eedd8
Wiktor Pink <wiktor.pink99@gmail.com>
2020-09-25 18:23:27 +0200

    Switching activities

--- app/src/main/java/com/pink/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/pink/swoosh/LeagueActivity.kt
@@ -1,11 +1,19 @@
 package com.pink.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_league.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LeagueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        returnButton.setOnClickListener {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
     }
 }
